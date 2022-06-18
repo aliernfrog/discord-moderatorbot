82e23d76e23c5139f3f09e517ad8c97566c46a81
uafl00iZeT8k917 <45766489+aliernfrog@users.noreply.github.com>
2022-06-18 13:13:48 +0000

    Fix no permissions warning

--- src/events/interactionCreate/contextMenu.js
+++ src/events/interactionCreate/contextMenu.js
@@ -4,7 +4,7 @@ module.exports = {
     if (!contextMenu) return interaction.reply({content: "Unknown context menu", ephemeral: true});
 
     if (contextMenu.guildOnly && !interaction.guild) return interaction.reply({content: "This command is guild-only", ephemeral: true});
-    if (contextMenu.permissions && !interaction.channel.permissionsFor(interaction.user).has(contextMenu.permissions)) return interaction.reply({content: `Missing permissions: \`${command.permissions.join("|")}\``, ephemeral: true});
+    if (contextMenu.permissions && !interaction.channel.permissionsFor(interaction.user).has(contextMenu.permissions)) return interaction.reply({content: `Missing permissions: \`${contextMenu.permissions.join("|")}\``, ephemeral: true});
 
     if (contextMenu.execute) contextMenu.execute(client, interaction);
   }
