60ad56b2cb92051e1f07ffb2a3a94f3a64a0e5e2
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-06-30 13:50:32 +0000

    Hardcode status guild count to 2

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,9 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    client.user.setActivity(`${client.guilds.cache.size} servers`, {type:"WATCHING"});
+    // hardcoded on purpose
+    // replace '2' with '${client.guilds.cache.size}' when needed
+    client.user.setActivity(`2 servers`, {type:"WATCHING"});
     console.log(`${client.user.tag} | ${client.guilds.cache.size} guilds`);
 
     process.on("uncaughtException", err => console.log(err));
