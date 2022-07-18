25d4c77f9f03396e32fe6fdddfc58d84b0b7f3ff
uafl00iZeT8k917 <45766489+aliernfrog@users.noreply.github.com>
2022-07-18 12:37:32 +0000

    refactor: remove status

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,11 +2,7 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    // hardcoded on purpose
-    // replace '2' with '${client.guilds.cache.size}' when needed
-    client.user.setActivity(`2 servers`, {type:"WATCHING"});
     console.log(`${client.user.tag} | ${client.guilds.cache.size} guilds`);
-
     process.on("uncaughtException", err => console.log(err));
   }
 }
