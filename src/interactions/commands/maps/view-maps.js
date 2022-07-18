b2e2d796fbd2ce1fd40b70f3ca68391952eab37b
uafl00iZeT8k917 <45766489+aliernfrog@users.noreply.github.com>
2022-07-18 14:46:39 +0000

    refactor: replace old constructors

--- src/interactions/commands/maps/view-maps.js
+++ src/interactions/commands/maps/view-maps.js
@@ -1,4 +1,4 @@
-const { MessageAttachment, MessageEmbed, PermissionFlagsBits } = require("discord.js");
+const { AttachmentBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
   data: {
@@ -12,11 +12,11 @@ module.exports = {
     const maps = data.maps || [];
     const string = JSON.stringify(maps, null, 1);
     if (string.length <= 4084) {
-      const embed = new MessageEmbed().setDescription("```js\n"+string+"\n```");
+      const embed = new EmbedBuilder().setDescription("```js\n"+string+"\n```");
       interaction.reply({embeds: [embed], ephemeral: true});
     } else {
       const buffer = Buffer.from(string, "utf-8");
-      const file = new MessageAttachment(buffer, "maps.json");
+      const file = new AttachmentBuilder(buffer, "maps.json");
       interaction.reply({files: [file], ephemeral: true});
     }
   }
