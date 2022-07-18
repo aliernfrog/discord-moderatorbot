a838bfe08dff4c45fd7e89e655f454e838c595cd
uafl00iZeT8k917 <45766489+aliernfrog@users.noreply.github.com>
2022-07-18 14:24:24 +0000

    refactor: replace method with ===

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,8 +1,9 @@
+const { InteractionType } = require("discord.js");
 const command = require("./interactionCreate/command.js");
 
 module.exports = {
   name: "interactionCreate",
   execute(client, interaction) {
-    if (interaction.isApplicationCommand()) command.execute(client, interaction);
+    if (interaction.type === InteractionType.ApplicationCommand) command.execute(client, interaction);
   }
 }
