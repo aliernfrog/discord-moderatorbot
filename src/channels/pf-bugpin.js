a21e4947912e06b4eec210dbd2bd5100e67996b3
a <45766489+aliernfrog@users.noreply.github.com>
2022-09-21 19:59:25 +0000

    fix: missing check for moderators

--- src/channels/pf-bugpin.js
+++ src/channels/pf-bugpin.js
@@ -1,6 +1,6 @@
 module.exports = {
   id: "1019668981457625189",
   execute(client, message) {
-    message.delete();
+    if (!message.author.moderator) message.delete();
   }
 }
--- src/channels/pf-dcpin.js
+++ src/channels/pf-dcpin.js
@@ -1,6 +1,6 @@
 module.exports = {
   id: "1019683152572190780",
   execute(client, message) {
-    message.delete();
+    if (!message.author.moderator) message.delete();
   }
 }
