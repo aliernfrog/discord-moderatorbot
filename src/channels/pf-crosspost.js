7062aeb0b6240aa2fa97c5f37af3d763513cee4c
a <45766489+aliernfrog@users.noreply.github.com>
2022-09-26 14:05:49 +0000

    fix: (crosspost) correct `catch` method

--- src/channels/pf-crosspost.js
+++ src/channels/pf-crosspost.js
@@ -5,20 +5,18 @@ module.exports = {
   execute(client, message) {
     if (!message.flags.has(MessageFlags.IsCrosspost)) return;
     const channel = client.channels.cache.get("855250425794920448");
-    
-    try {
-      channel.send({
-        allowedMentions: { parse: [] },
-        content: message.content,
-        embeds: message.embeds,
-        files: message.attachments?.map(a => AttachmentBuilder.from(a))
-      }).then(msg => msg.crosspost());
-    } catch (e) {
+
+    channel.send({
+      allowedMentions: { parse: [] },
+      content: message.content,
+      embeds: message.embeds,
+      files: message.attachments?.map(a => AttachmentBuilder.from(a))
+    }).then(msg => msg.crosspost()).catch(() => {
       channel.send({
         allowedMentions: { parse: [] },
         content: message.content+message.attachments?.map(a => `\n${a.url}`),
         embeds: message.embeds
       }).then(msg => msg.crosspost());
-    }
+    });
   }
 }
