db15038f4d5f755f5a66f8f861270a876f4e251f
a <45766489+aliernfrog@users.noreply.github.com>
2023-01-15 20:47:11 +0000

    chore: update dependencies

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("../config.js");
+mongoose.set("strictQuery", true);
 
 const Guild = require("./schemas/Guild.js");
 const User = require("./schemas/User.js");
