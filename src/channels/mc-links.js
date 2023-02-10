f9e9b0645adced9437afd7c3c19ac74a95858dc8
a <45766489+aliernfrog@users.noreply.github.com>
2023-02-10 20:16:15 +0000

    feat(mc/links): update rules

--- src/channels/mc-links.js
+++ src/channels/mc-links.js
@@ -1,8 +1,8 @@
 module.exports = {
   id: "855252920961728552",
-  linksOnly: true,
+  linksOnly: true/*,
   cooldown: {
     duration: 604800000,
     message: "You have already advertised this week, you have to wait until %TIME% to advertise again"
-  }
+  }*/
 }
