466abe1340d783594f99f204e0f0336f4d61d907
a <45766489+aliernfrog@users.noreply.github.com>
2023-04-02 19:50:48 +0000

    feat: remove unneeded channels

--- src/channels/pf-bugpin.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  id: "1019668981457625189",
-  execute(client, message) {
-    if (!message.author.moderator) message.delete();
-  }
-}
--- src/channels/pf-dcpin.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  id: "1019683152572190780",
-  execute(client, message) {
-    if (!message.author.moderator) message.delete();
-  }
-}
