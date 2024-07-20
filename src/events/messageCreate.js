3b9d3228bae034b5e132eb498b3cd615e75e2a9e
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-07-20 16:10:49 +0300

    delete thread created messages in faq channels if sent by bot

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,7 @@
 import handleSpecialChannel from "./messageCreate/specialChannels.js";
 import handleNavigatorChannel from "./messageCreate/navigatorChannels.js";
 import handleManagerEval from "./messageCreate/managerEval.js";
+import handleFAQChannels from "./messageCreate/faqChannels.js";
 
 export default {
   name: "messageCreate",
@@ -8,5 +9,6 @@ export default {
     handleSpecialChannel(client, message);
     handleNavigatorChannel(client, message);
     handleManagerEval(client, message);
+    handleFAQChannels(client, message);
   }
 }
